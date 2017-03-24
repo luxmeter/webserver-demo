4c1d2681362b3c08a5f8a056672dabeb5effa7b4
luxmeter <superhans@ludete.de>
2017-03-24 21:55:59 +0100

    added start script

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/env sh
+
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+EXECUTABLE=$(find $SCRIPTPATH/target/*.jar 2> /dev/null)
+
+if [[ $? != 0 ]]; then
+	echo "Executable not found :("
+else
+	echo "Executing $EXECUTABLE"
+	java -jar $EXECUTABLE
+fi
