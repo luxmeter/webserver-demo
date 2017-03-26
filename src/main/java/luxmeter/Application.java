56de0b3ebdf1e11c097c413205e2cfc97011ee85
luxmeter <superhans@ludete.de>
2017-03-27 00:06:25 +0200

    added help page and argument processing

--- src/main/java/luxmeter/Application.java
+++ src/main/java/luxmeter/Application.java
@@ -1,37 +1,76 @@
 package luxmeter;
 
-import com.sun.net.httpserver.HttpContext;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
-import luxmeter.filter.IfMatchFilterWithoutStar;
-import luxmeter.filter.IfNoneMatchFilterWithoutStar;
-import luxmeter.filter.ModifiedSinceFilter;
-import luxmeter.filter.RequestValidationFilter;
-import luxmeter.handler.ContextManager;
-import luxmeter.handler.DefaultHandler;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.InetSocketAddress;
+import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.concurrent.Executors;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.asList;
 
 public class Application {
+    private static final String HELP_FILE = "/help.txt";
+    private static final String OPT_ROOT_DIR = "--rootdir";
+    private static final String OPT_HELP = "--help";
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+    private static final HashSet<String> ALLOWED_OPTIONS = new HashSet<>(asList(OPT_ROOT_DIR, OPT_HELP));
+
     public static void main(String[] args) throws IOException {
-        // default implementation is already able to handle keep-alive requests
-        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-        Path rootDir = Paths.get(System.getProperty("user.dir"));
-        HttpHandler handler = new DefaultHandler(rootDir);
-        // handles RequestExceptions and frees Input- and Output-streams
-        handler = ContextManager.decorate(handler);
-        HttpContext context = server.createContext("/", handler);
-        // TODO add spring injection to dynamically load filterer
-        context.getFilters().add(new RequestValidationFilter(rootDir));
-        context.getFilters().add(new ModifiedSinceFilter(rootDir));
-        context.getFilters().add(new IfNoneMatchFilterWithoutStar(rootDir));
-        context.getFilters().add(new IfMatchFilterWithoutStar(rootDir));
-        server.setExecutor(Executors.newCachedThreadPool());
-        server.start();
+        List<String> options = checkArguments(args);
+        Path rootDir = getRootDir(options);
+        System.out.println("Root Directory: " + rootDir);
+        System.out.println(String.format("Ready to serve: %s\n", "http://localhost:8080/"));
+        new Server(rootDir).start();
+    }
+
+    private static List<String> checkArguments(String[] args) throws IOException {
+        List<String> arguments = asList(args);
+        Set<String> options = arguments.stream()
+                // make '--rootDir=something' to '--rootDir'
+                // or something invalid --> in this case the help page is presented
+                .map(arg -> arg.trim().replaceAll("(--.*)=.*", "$1"))
+                .collect(Collectors.toSet());
+        if (!arguments.isEmpty()
+                && (arguments.contains(OPT_HELP) || !ALLOWED_OPTIONS.containsAll(options))) {
+            printHelpPage();
+            System.exit(1);
+        }
+        return arguments;
+    }
+
+    private static Path getRootDir(Collection<String> arguments) {
+        Path path = arguments.stream()
+                .filter(option -> option.startsWith(OPT_ROOT_DIR))
+                .map(option -> option.substring(OPT_ROOT_DIR.length() + 1))
+                // in case someone wrote --rootDir='something'
+                .map(option -> option.replaceAll("['|\"]", StringUtils.EMPTY))
+                .map(Paths::get)
+                .findFirst()
+                .orElse(Paths.get(System.getProperty("user.dir")));
+        if (!path.toFile().isDirectory()) {
+            System.err.println("Don\'t fool me. Enter an existing directory");
+            System.exit(1);
+        }
+        return path;
+    }
+
+    private static void printHelpPage() throws IOException {
+        BufferedReader reader =
+                new BufferedReader(new InputStreamReader(Application.class.getResourceAsStream(HELP_FILE)));
+        StringBuilder output = new StringBuilder();
+        for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+            output.append(line + "\n");
+        }
+        System.out.println(output.toString());
     }
 
 }
---
+++ src/main/java/luxmeter/Server.java
@@ -0,0 +1,44 @@
+package luxmeter;
+
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import luxmeter.filter.IfMatchFilterWithoutStar;
+import luxmeter.filter.IfNoneMatchFilterWithoutStar;
+import luxmeter.filter.ModifiedSinceFilter;
+import luxmeter.filter.RequestValidationFilter;
+import luxmeter.handler.ContextManager;
+import luxmeter.handler.DefaultHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.file.Path;
+import java.util.concurrent.Executors;
+
+public class Server {
+    private static final Logger logger = LoggerFactory.getLogger(Server.class);
+    private final Path rootDir;
+
+    public Server(Path rootDir) {
+        this.rootDir = rootDir;
+    }
+
+    public void start() throws IOException {
+        // default implementation is already able to handle keep-alive requests
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        HttpHandler handler = new DefaultHandler(rootDir);
+        // handles RequestExceptions and frees Input- and Output-streams
+        handler = ContextManager.decorate(handler);
+        HttpContext context = server.createContext("/", handler);
+        // TODO add spring injection to dynamically load filterer
+        context.getFilters().add(new RequestValidationFilter(rootDir));
+        context.getFilters().add(new ModifiedSinceFilter(rootDir));
+        context.getFilters().add(new IfNoneMatchFilterWithoutStar(rootDir));
+        context.getFilters().add(new IfMatchFilterWithoutStar(rootDir));
+        server.setExecutor(Executors.newCachedThreadPool());
+
+        server.start();
+    }
+}
