6c53ce132181e4440490d23877380c6a5176a00e
Mustafa Caylak <mustafa@ludete.de>
2017-03-29 09:59:04 +0200

    Update HeaderFieldContants.java

--- src/main/java/luxmeter/model/HeaderFieldContants.java
+++ src/main/java/luxmeter/model/HeaderFieldContants.java
@@ -6,6 +6,5 @@ public class HeaderFieldContants {
     public static final String IF_MODIFIED_SINCE = "If-Modified-Since";
     public static final String ETAG = "Etag";
     public static final String CONNECTION = "Connection";
-    // spell it exactly so otherwise chrome does not recognize the field
-    public static final String CONTENT_TYPE = "Content-type";
+    public static final String CONTENT_TYPE = "Content-Type";
 }
